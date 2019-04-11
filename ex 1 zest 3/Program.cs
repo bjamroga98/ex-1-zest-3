244ad7d7e04145b4f6266f0a1e809f6831d3c8ef
barbara.jamroga <barbara.jamroga@microsoft.wsei.edu.pl>
2019-04-11 09:10:18 +0200

    Commit

--- ex 1 zest 3/Program.cs
+++ ex 1 zest 3/Program.cs
@@ -10,6 +10,30 @@ namespace ex_1_zest_3
     {
         static void Main(string[] args)
         {
+            string text1 = "Itsy Bitsy Spider";
+            string text2 = TrimSpaces(text1);
+            Console.WriteLine("Before: " + text1);
+            Console.WriteLine("After: " + text2);
+
+            Console.ReadKey();
+
+            /**********************************************/
+
+           
+           
+        }
+        static string TrimSpaces(string text)
+        {
+            // The final string to store the shortened text
+            string textWithoutSpaces = "";
+
+            for (int i = 0; i < text.Length; ++i)
+            {
+                //if a given char not empty , add it to the final string
+                if (text[i] != ' ' && text[i] != '\t' && text[i] != '\n')
+                    textWithoutSpaces += text[i];
+            }
+            return textWithoutSpaces;
         }
     }
 }
